28d02940bf7d98cad8c5256a5429eb821ec3c5f8
Avishek Devnath <avishekdevnath@gmail.com>
2022-02-14 13:26:09 +0600

    a

--- src/Components/Login/Login/Firebase/Firebase.config.js
+++ src/Components/Login/Login/Firebase/Firebase.config.js
@@ -7,12 +7,6 @@ const firebaseConfig = {
     messagingSenderId: "67504916362",
     appId: "1:67504916362:web:c5e6990eed3f2f67c6ea73"
 
-    // apiKey: "AIzaSyAUPVQK3wdo0hcdU1AjOlWgzFs00xOPu9A",
-    // authDomain: "travel-blog-ba0c6.firebaseapp.com",
-    // projectId: "travel-blog-ba0c6",
-    // storageBucket: "travel-blog-ba0c6.appspot.com",
-    // messagingSenderId: "832925418003",
-    // appId: "1:832925418003:web:4f8ce723ba835dbc9bb5e6"
 };
 
 export default firebaseConfig;
